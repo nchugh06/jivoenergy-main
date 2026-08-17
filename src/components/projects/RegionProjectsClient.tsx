058bd52124089df5c1cd6e582254f3223b16ea8b
shubhamjivo <shubham@jivosolutions.com>
2026-08-17 13:04:17 +0530

    reomve object fit from banner

--- src/components/projects/RegionProjectsClient.tsx
+++ src/components/projects/RegionProjectsClient.tsx
@@ -67,7 +67,6 @@ export default function RegionProjectsClient({ regionId }: RegionProjectsClientP
             src={region.bannerMobile}
             alt={`${region.label} Projects`}
             fill
-            className="object-cover"
             priority
           />
         </div>
